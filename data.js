ceef5574aa3f28fe2766e2ddd1ad78f05ee8735a
aechevarriat <aliciaechevarria27@gmail.com>
2023-05-26 11:31:05 +0200

    added files

---
+++ data.js
@@ -0,0 +1,466 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vestibulo-lounge",
+      "name": "VESTIBULO LOUNGE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": -2.1905243552312896,
+        "pitch": 0.04891873616364073,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1056791716675,
+          "pitch": 0.12490722536123755,
+          "rotation": 0,
+          "target": "11-pasillo-cocina"
+        },
+        {
+          "yaw": -1.1261020695500061,
+          "pitch": 0.1932606358362019,
+          "rotation": 0,
+          "target": "3-barra-lounge"
+        },
+        {
+          "yaw": -0.0005561132417888359,
+          "pitch": 0.20708912117628664,
+          "rotation": 0,
+          "target": "2-bao-cafeteria"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-vestibulo-restaurante",
+      "name": "VESTIBULO RESTAURANTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": 0.5596725167383205,
+        "pitch": 0.06591773391321532,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4624916286063119,
+          "pitch": 0.1434789603811133,
+          "rotation": 0,
+          "target": "9-pasillo-servicios"
+        },
+        {
+          "yaw": -0.07176699020483923,
+          "pitch": 0.196948518927476,
+          "rotation": 0,
+          "target": "10-restaurante"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bao-cafeteria",
+      "name": "BAÑO CAFETERIA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.12947169723885388,
+        "pitch": 0.0885350198903776,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.48991216846907903,
+          "pitch": 0.3291625975083541,
+          "rotation": 0.7853981633974483,
+          "target": "0-vestibulo-lounge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-barra-lounge",
+      "name": "BARRA LOUNGE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -0.3243808348633799,
+        "pitch": 0.0838353575107682,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7751680221262944,
+          "pitch": 0.23649037028411257,
+          "rotation": 0,
+          "target": "0-vestibulo-lounge"
+        },
+        {
+          "yaw": 1.8458720618464781,
+          "pitch": 0.2686052650918711,
+          "rotation": 0,
+          "target": "6-lounge-sofa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cafeteria",
+      "name": "CAFETERIA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": -2.59807352444909,
+        "pitch": 0.09738400943930436,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3461110813863435,
+          "pitch": 0.23086697907656983,
+          "rotation": 0,
+          "target": "6-lounge-sofa"
+        },
+        {
+          "yaw": -1.1775639259215538,
+          "pitch": 0.1766157920525817,
+          "rotation": 0,
+          "target": "5-cafeteria1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-cafeteria1",
+      "name": "CAFETERIA1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": -0.9711794798399076,
+        "pitch": 0.05025216239107522,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9849571754965254,
+          "pitch": 0.4078975387405386,
+          "rotation": 0,
+          "target": "4-cafeteria"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-lounge-sofa",
+      "name": "LOUNGE SOFA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": 0.08472780186952633,
+        "pitch": 0.2799194855250633,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.335200331829956,
+          "pitch": 0.3520879444423599,
+          "rotation": 0,
+          "target": "3-barra-lounge"
+        },
+        {
+          "yaw": 1.3428000179821158,
+          "pitch": 0.44012448579071517,
+          "rotation": 0,
+          "target": "4-cafeteria"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-parteluz",
+      "name": "PARTELUZ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": 0.051407879786012245,
+        "pitch": 0.27471895877749297,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0908503759375066,
+          "pitch": 0.4448838784808178,
+          "rotation": 0,
+          "target": "10-restaurante"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pasillo-lounge-bar",
+      "name": "PASILLO LOUNGE BAR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pasillo-servicios",
+      "name": "PASILLO SERVICIOS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": 2.8721256944797853,
+        "pitch": 0.17001343301313376,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2323977563498403,
+          "pitch": 0.15080057434495941,
+          "rotation": 0,
+          "target": "11-pasillo-cocina"
+        },
+        {
+          "yaw": -1.891365513196778,
+          "pitch": 0.19966338013502138,
+          "rotation": 0,
+          "target": "1-vestibulo-restaurante"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-restaurante",
+      "name": "RESTAURANTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": 0.032367924309717466,
+        "pitch": 0.14061444335531448,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1278398325836108,
+          "pitch": 0.3793362150866706,
+          "rotation": 0,
+          "target": "7-parteluz"
+        },
+        {
+          "yaw": 1.7997590050694878,
+          "pitch": 0.25261698367507,
+          "rotation": 0,
+          "target": "1-vestibulo-restaurante"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pasillo-cocina",
+      "name": "PASILLO COCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 843.5,
+      "initialViewParameters": {
+        "yaw": -0.6311730035249212,
+        "pitch": 0.011717870279612796,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.009612878144613646,
+          "pitch": 0.10617103930468019,
+          "rotation": 0,
+          "target": "0-vestibulo-lounge"
+        },
+        {
+          "yaw": -3.139378971730739,
+          "pitch": 0.100275212441721,
+          "rotation": 0,
+          "target": "9-pasillo-servicios"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ES SINDICAT",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
